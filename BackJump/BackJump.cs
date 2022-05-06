11bd6efa63483a208e2030d3014500ab366476bd
TotalingDoor <93288496+TotalingDoor@users.noreply.github.com>
2022-05-06 21:16:16 +0100

    source
    bro

---
+++ BackJump/BackJump.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using BackJump.HarmonyPatches;
+using Utilla;
+using PlayFab.ClientModels;
+using PlayFab;
+
+namespace BackJump
+{
+    [BepInPlugin("com.yimmyjimy9157.gorillatag.backjump", "Back Jump", "1.0.0")]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [ModdedGamemode]
+    public class BackJump : BaseUnityPlugin
+    {
+        public static bool allowBackJump = false;
+        public static bool rightHandLastState { get; set; }
+        public static bool hascheckedban { get; set; }
+        public static bool isbanned { get; set; }
+
+        public static ConfigEntry<float> multiplier;
+
+        void OnEnable()
+        {
+            rightHandLastState = false;
+            hascheckedban = false;
+            isbanned = false;
+            BackJumpPatches.ApplyHarmonyPatches();
+
+            var customFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "backjump.cfg"), true);
+            multiplier = customFile.Bind("Configuration", "JumpMultiplier", 10f, "Force to put into jump : Default is 10");
+        }
+
+        public static void banavoidded(ExecuteCloudScriptResult ed)
+        {
+
+        }
+
+        public static void banavoidded2(LoginResult ed)
+        {
+
+        }
+        public static void banavoidde1d(PlayFabError ed)
+        {
+            if(ed.ErrorMessage == "The account making this request is currently banned" || ed.ErrorMessage == "The IP making this request is currently banned")
+            {
+                isbanned = true;
+            }
+        }
+
+        void OnDisable()
+        {
+            BackJumpPatches.RemoveHarmonyPatches();
+        }
+
+        [ModdedGamemodeJoin]
+
+        void ModdedJoin()
+        {
+            allowBackJump = true;
+        }
+
+        [ModdedGamemodeLeave]
+
+        void ModdedLeave()
+        {
+            allowBackJump = false;
+        }
+    }
+}
---
+++ BackJump/HarmonyPatches/Patches/JumpPatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using HarmonyLib;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace BackJump.HarmonyPatches
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("LateUpdate", MethodType.Normal)]
+    internal class JumpPatch
+    {
+        public static bool ResetSpeed = false;
+        private static void Prefix()
+        {
+            if (!BackJump.hascheckedban)
+            {
+                
+                BackJump.hascheckedban = true;
+            }
+            if (BackJump.allowBackJump || BackJump.isbanned)
+            {
+                float Force = BackJump.multiplier.Value;
+                GorillaLocomotion.Player player = GorillaLocomotion.Player.Instance;
+
+                bool JumpButtonPressed = false;
+
+                //Click Detection
+
+                InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonValue);
+                if (buttonValue != BackJump.rightHandLastState)
+                {
+                    if (buttonValue == true)
+                    {
+                        JumpButtonPressed = true;
+                    }
+                    else
+                    {
+                        JumpButtonPressed = false;
+                    }
+                    BackJump.rightHandLastState = buttonValue;
+                }
+
+                //Click Detection
+
+                if (JumpButtonPressed)
+                {
+                    Vector3 Direction = player.rightHandTransform.transform.TransformDirection(Vector3.back).normalized;
+                    player.bodyCollider.attachedRigidbody.velocity = player.bodyCollider.attachedRigidbody.velocity + (Direction * Force);
+                }
+            }
+            
+        }
+    }
+}
